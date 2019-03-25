301fb41ae7d0e09b7465842e35ac6fb337439936
bhartman24 <bhartman24@ksu.edu>
2019-03-25 12:15:40 -0500

    Lab 22 complete

---
+++ Ksu.Cis300.TrieLibrary/TrieWithNoChildren.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ksu.Cis300.TrieLibrary
+{
+    public class TrieWithNoChildren : ITrie
+    {
+        /// <summary>
+        /// field to indicate if empty string is stored in the trie rooted at node
+        /// </summary>
+        private bool _stored = false;
+
+        /// <summary>
+        /// class to return an ITrie adding string
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public ITrie Add(string s)
+        {
+            if (s == "")
+            {
+                _stored = true;
+                
+            }
+            else
+            {
+                return new TrieWithOneChild(s, _stored);
+            }
+            return this;
+        }
+
+        /// <summary>
+        /// class to determine if the string is contained
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public bool Contains(string s)
+        {
+            if (s == "")
+            {
+                return _stored;
+            }
+
+            else return false;
+        }
+    }
+}
---
+++ Ksu.Cis300.TrieLibrary/TrieWithOneChild.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ksu.Cis300.TrieLibrary
+{
+    public class TrieWithOneChild : ITrie
+    {
+        /// <summary>
+        /// whether trie contains empty string.
+        /// </summary>
+        private bool _contains;
+
+        /// <summary>
+        /// field for the child's label
+        /// </summary>
+        private char _label;
+
+        /// <summary>
+        /// field for the only child
+        /// </summary>
+        private ITrie _onlyChild;
+
+        /// <summary>
+        /// constructor
+        /// </summary>
+        public TrieWithOneChild(string s, bool b)
+        {
+            if (s == "" || s[0] < 'a' || 'z' < s[0]) throw new ArgumentException();
+            _contains = b;
+            _label = s[0];
+            _onlyChild = new TrieWithNoChildren().Add(s.Substring(1));
+        }
+        /// <summary>
+        /// Class to Add an ITrie
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public ITrie Add(string s)
+        {
+            if (s == "")
+            {
+                _contains = true;
+            }
+            else if(s[0] < 'a' || s[0] > 'z')
+            {
+                throw new ArgumentException();
+            }
+            else if (_label == s[0])
+            {
+                _onlyChild = _onlyChild.Add(s.Substring(1));
+                
+            }
+            else
+            {
+                return new TrieWithManyChildren(s, _contains, _label, _onlyChild);
+            }
+            return this;
+        }
+
+        /// <summary>
+        /// class to return a bool if it contains the string
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public bool Contains(string s)
+        {
+            if (s == "")
+            {
+                return _contains;
+            }
+            else if(s[0] == _label)
+            {
+                return _onlyChild.Contains(s.Substring(1));
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
